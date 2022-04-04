e7a9e6f15f31488ba8782253f9031c44256fa87d
Shafique Ahmad <scmnst@yahoo.com>
2022-04-04 05:31:29 +0500

    removed commets

--- index.php
+++ index.php
@@ -55,23 +55,7 @@ foreach($rs as $r){
 
 </div>
 </div>
-<script>/*
-$(document).ready(function (){
-var audioArray = document.getElementsByClassName('numbers');
-var i = 0;
-audioArray[i].play();
-for (i = 0; i < audioArray.length - 1; ++i) {
-audioArray[i].addEventListener('ended', function(e){
-	var currentSong = e.target;
-	var next = $(currentSong).nextAll('audio');
-	if (next.length) $(next[0]).trigger('play');
-});
-}
-});
-*/
-
-
-
+<script>
 var audioArray = document.getElementsByClassName('numbers');
 var i = 0;
 audioArray[i].play();
